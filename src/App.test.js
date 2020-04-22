f31b3d3372366026b67b0c8a84bfc769b3799e06
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-04-22 07:25:15 -0700

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,27 @@
-import '@testing-;ibrary/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 import React from "react";
 import App from "./App";
 import * as rtl from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
+import {render} from "@testing-library/react"
 
 describe ("App", () => {
-    let wrapper;
-    afterEach(rtl.cleanup);
-    beforeEach(() => {
-        wrapper =rtl.render(<App />)
-    })
+    // let wrapper;
+    // afterEach(rtl.cleanup);
+    // beforeEach(() => {
+        // wrapper =render(<App />)
+    // })
 
     it ("displays the correct name", async () => {
-        const text = await wrapper.findByText("Select a szn");
+        const { findByText, debug, getAllByText} = render(<App />)
+        const text = await findByText("Select a season");
         expect(text).toBeInTheDocument();
         expect(text).toBeVisible();
+        userEvent.click(text)
+        debug();
+        expect(getAllByText(/season /i)).toHaveLength(4)
     })
+
+
+
 });
